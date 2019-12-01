118894d7b1ab7c04be2bd14a6b2001b4d735b47d
doctorblinch <docblinch@gmail.com>
2019-12-01 17:17:20 +0200

    login + ordinary staff

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,47 @@
-from app import app
+from flask_login import current_user, login_user, logout_user, login_required
+from flask import render_template, flash, redirect, url_for, jsonify
+from app import app, db
+from app.errors import *
+from app.models import User
+from app.forms import RegistrationForm, LoginForm
+
 
 @app.route('/')
 def index():
-    return 'Hellow world!'
+    return render_template('index.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
--- config.py
+++ config.py
@@ -1,4 +1,14 @@
 import os
 
+POSTGRES_URL = os.environ.get("POSTGRES_URL")
+POSTGRES_USER = os.environ.get("POSTGRES_USER")
+POSTGRES_PW = os.environ.get("POSTGRES_PW")
+POSTGRES_DB = os.environ.get("POSTGRES_DB")
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fennec-fox-fit'
+    SQLALCHEMY_DATABASE_URI = DB_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
